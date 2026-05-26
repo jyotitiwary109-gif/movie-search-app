0aa8ac025d7ad213cfbf5ae1bd0bf6ddc8323574
jyotitiwary109-gif <jyotitiwary109@gmail.com>
2026-05-26 07:00:47 +0530

    Add files via upload
    Added Movie Search App

---
+++ README.md
@@ -0,0 +1,15 @@
+# Movie Search App
+
+A simple Movie Search App built using HTML, CSS, and JavaScript.
+
+## Features
+- Search movies by name
+- Fetch movie data from OMDb API
+- Responsive movie cards
+- Loading and error handling
+
+## Technologies Used
+- HTML
+- CSS
+- JavaScript
+- OMDb API
---
+++ script.js
@@ -0,0 +1,54 @@
+const API_KEY="857019df";
+const searchBtn=document.getElementById("searchBtn");
+const searchInput = document.getElementById("searchInput");
+const movieContainer = document.getElementById("movieContainer");
+searchBtn.addEventListener("click",searchMovies);
+async function searchMovies() {
+    const movieName=searchInput.value.trim();
+    if (movieName==="") {
+        movieContainer.innerHTML="<h2>Please enter movie name</h2>";
+        return;
+    }
+    movieContainer.innerHTML="<h2>Loading...</h2>";
+    
+  try {
+
+    const response = await fetch(
+      `https://www.omdbapi.com/?s=${movieName}&apikey=${API_KEY}`
+    );
+
+    const data = await response.json();
+
+    if (data.Response === "False") {
+      movieContainer.innerHTML = `<h2>${data.Error}</h2>`;
+      return;
+    }
+
+    showMovies(data.Search);
+
+  } catch (error) {
+
+    movieContainer.innerHTML = "<h2>Something went wrong</h2>";
+
+  }
+}
+function showMovies(movies) {
+    movieContainer.innerHTML="";
+    movies.forEach(movie => {
+        const movieCard=document.createElement("div");
+         movieCard.classList.add("movie-card");
+
+    const poster =
+      movie.Poster !== "N/A"
+        ? movie.Poster
+        : "https://via.placeholder.com/300x450?text=No+Image";
+        movieCard.innerHTML=`<img src ="${movie.Poster}" alt="${movie.Title}">
+        <div class="movie-info">
+        <h2>${movie.Title}</h2>
+        <p>Year:${movie.Year}</p>
+        <p>Type:${movie.Type}</p>
+        </div>
+        `;
+        movieContainer.appendChild(movieCard);
+    });
+}
